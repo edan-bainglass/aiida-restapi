eb6954c0d2eabfcd17179fb3b3ef11790839b789
Edan Bainglass <45081142+edan-bainglass@users.noreply.github.com>
2026-07-29 12:18:45 +0200

    Return QB results in dict format (#147)
    `QueryBuilder().dict()` returns a more general result form that is
    easier to work with, particularly as it returns projection field keys
    along with values.

--- tests/test_querybuilder.py
+++ tests/test_querybuilder.py
@@ -40,37 +40,12 @@ def test_querybuilder_full(client: TestClient):
         },
     )
     assert response.status_code == 200, response.text
-    result = response.json()['data']['attributes']['results'][0]
+    result = response.json()['data']['attributes']['results'][0]['integer']
     assert 'attributes' in result
     assert 'value' in result['attributes']
     assert result['attributes']['value'] == 1
     assert 'extras' in result
     assert 'repository_metadata' in result
-
-
-@pytest.mark.usefixtures('default_nodes')
-def test_querybuilder_flat(client: TestClient):
-    """Test QueryBuilder result with flat response."""
-    response = client.post(
-        '/querybuilder?flat=true',
-        json={
-            'path': [
-                {
-                    'entity_type': ['data.core.int.Int.', 'data.core.float.Float.'],
-                    'orm_base': 'node',
-                    'tag': 'nodes',
-                },
-            ],
-            'project': {
-                'nodes': [
-                    'attributes.value',
-                ],
-            },
-        },
-    )
-    assert response.status_code == 200, response.text
-    result = response.json()
-    assert result['data']['attributes']['results'] == [1, 1.1]
 
 
 def test_querybuilder_node_in_group(client: TestClient, default_nodes: list[str], default_groups: list[str]):
@@ -113,4 +88,83 @@ def test_querybuilder_node_in_group(client: TestClient, default_nodes: list[str]
     )
     assert response.status_code == 200, response.text
     result = response.json()['data']['attributes']['results']
-    assert result == [group.label, node.pk, node.value]
+    assert result == [
+        {
+            'group': {
+                'label': group.label,
+            },
+            'node': {
+                'pk': node.pk,
+                'attributes.value': node.value,
+            },
+        }
+    ]
+
+
+def _format_time(time: str) -> str:
+    """Format a time string to ISO 8601 format with 'Z' suffix for UTC."""
+    return time.isoformat().replace('+00:00', 'Z')
+
+
+def test_querybuilder_result_normalization(client: TestClient, default_nodes: list[str], default_groups: list[str]):
+    """Test the normalization of QueryBuilder results."""
+    node = orm.load_node(default_nodes[0])
+    group = orm.load_group(default_groups[0])
+    group.add_nodes(node)
+    response = client.post(
+        '/querybuilder?flat=true',
+        json={
+            'path': [
+                {
+                    'entity_type': 'group.core.',
+                    'orm_base': 'group',
+                    'tag': 'group',
+                },
+                {
+                    'entity_type': 'data.core.int.Int.',
+                    'orm_base': 'node',
+                    'joining_keyword': 'with_group',
+                    'joining_value': 'group',
+                    'tag': 'node',
+                },
+            ],
+            'filters': {
+                'group': {
+                    'pk': group.pk,
+                }
+            },
+            'project': {
+                'group': [
+                    '*',
+                ],
+                'node': [
+                    '*',
+                ],
+            },
+        },
+    )
+    assert response.status_code == 200, response.text
+    result = response.json()['data']['attributes']['results']
+    assert result == [
+        {
+            'group': {
+                'pk': group.pk,
+                'uuid': str(group.uuid),
+                'type_string': group.type_string,
+                'label': group.label,
+                'description': group.description,
+                'time': _format_time(group.time),
+                'user': group.user.id,
+            },
+            'node': {
+                'pk': node.pk,
+                'uuid': str(node.uuid),
+                'node_type': node.node_type,
+                'label': node.label,
+                'description': node.description,
+                'ctime': _format_time(node.ctime),
+                'mtime': _format_time(node.mtime),
+                'user': node.user.id,
+            },
+        }
+    ]
